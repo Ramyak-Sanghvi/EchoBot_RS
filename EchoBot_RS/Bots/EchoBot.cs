17d8c8ff02cd6183a99c8823a97aca0436dcb3c2
Ramyak Sanghvi <ramyaksanghvi@gmail.com>
2020-09-18 16:19:20 +0530

    Google Protobuf Demo Added

--- EchoBot_RS/Bots/EchoBot.cs
+++ EchoBot_RS/Bots/EchoBot.cs
@@ -3,9 +3,12 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.10.3
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Google.Protobuf;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
@@ -15,7 +18,24 @@ namespace EchoBot_RS.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            string[] values = turnContext.Activity.Text.Split(',');
+            TeamsText.Response text = new TeamsText.Response();
+            text.Id = Convert.ToInt32(values[0].Trim());
+            text.Text = values[1].Trim();
+            using (var output = File.Create("teamsText.dat"))
+            {
+                text.WriteTo(output);
+            }
+            TeamsText.Response text1;
+            using (var input = File.OpenRead("teamsText.dat"))
+            {
+                text1 = TeamsText.Response.Parser.ParseFrom(input);
+            }
+            Console.WriteLine(text1.Id);
+            Console.WriteLine(text1.Text);
+
             var replyText = turnContext.Activity.From.Name + ": " + turnContext.Activity.Text;
+            Console.WriteLine(replyText);
             await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
         }
     }
